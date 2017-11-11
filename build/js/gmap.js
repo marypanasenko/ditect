c73bef7bd8bfcb64935723cd1269598e41d610cf
Maria Panasenko <kerch.pm@gmail.com>
2017-11-11 12:13:57 +0300

    /build

---
+++ build/js/gmap.js
@@ -0,0 +1,16 @@
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 7,
+//     center: new google.maps.LatLng(34.8692161,-111.7651565)
+//   }
+//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+//                                 mapOptions);
+//   var image = 'img/icon-map-marker.svg';
+//   var myLatLng = new google.maps.LatLng(34.8692161,-111.7651565);
+//   var beachMarker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     icon: image
+//   });
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
