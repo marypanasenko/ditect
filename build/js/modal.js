c0679026f22cfcf753d2cef16356ee5a405a71e0
Maria Panasenko <kerch.pm@gmail.com>
2017-12-11 13:53:21 +0300

    add code for modal window

--- build/js/modal.js
+++ build/js/modal.js
@@ -1,28 +1,14 @@
-// var modalFailure = document.querySelector('.modal__failure');
-// var modalSuccess = document.querySelector('.modal__success');
-// var form = document.querySelector('.form');
-// var closeOk = document.querySelector('.modal__button--ok');
-// var closeSuccess = document.querySelector('.modal__button--success');
-//
-// form.addEventListener('invalid', function(e) {
-//   e.preventDefault();
-//
-//   modalFailure.classList.add('modal__failure--showed');
-// }, true);
-//
-// form.addEventListener('submit', function(e) {
-//   e.preventDefault();
-//
-//   modalFailure.classList.remove('modal__failure--showed');
-//   modalSuccess.classList.add('modal__success--showed');
-// });
-//
-// closeOk.addEventListener('click', function(e) {
-//   event.preventDefault();
-//   modalFailure.classList.remove('modal__failure--showed');
-// });
-//
-// closeSuccess.addEventListener('click', function(e) {
-//   event.preventDefault();
-//   modalSuccess.classList.remove('modal__success--showed');
-// });
+var modalButtonclose = document.querySelector('.modal-services__button');
+var modalRepair = document.querySelector('.modal-services__repair');
+var buttonRepair = document.querySelector('.repair');
+
+
+buttonRepair.addEventListener('click', function() {
+    event.preventDefault();
+    modalRepair.classList.add("modal-services__repair--show");
+});
+
+modalButtonclose.addEventListener('click', function() {
+    event.preventDefault();
+    modalRepair.classList.remove("modal-services__repair--show");
+});
